262959d5df3e2ce4fd95b7a66d1b6142df14f12b
RocioGarxia <rociogarciacrespo@gmail.com>
2021-06-14 20:03:05 -0300

    solo en la parte de contacto agregué un document ready con un click y un set timeout para el modal del formulario

--- COMPONENTS/app.js
+++ COMPONENTS/app.js
@@ -1,3 +1,4 @@
+/*
 let carritoDeCompras = []
 const contenedorProducto = document.getElementById('contenedor-productos')
 const contenedorCarrito = document.getElementById('carrito-contenedor')
@@ -8,7 +9,9 @@ const precioTotal = document.getElementById('precioTotal')
 mostrarProductos(stockProductos)
 
 function mostrarProductos(array) {
-    array.forEach((x) => {
+   // array.forEach((x) => { //
+   contenedorProductos.innerHTML = ''
+    array.forEach((productoNuevo)=>{
         let div = document.createElement('div')
         div.classList.add('producto')
         div.innerHTML += `
@@ -24,23 +27,44 @@ function mostrarProductos(array) {
 
         contenedorProducto.appendChild(div)
 
-        let boton = document.getElementById(`${x.id}`)
-        boton.addEventListener('click', () => { agregarAlCarrito(`${x.id}`) })
+       // let boton = document.getElementById(`${x.id}`) //
 
-    });
+       let boton = document.getElementById(`boton${productoNuevo.id}`)
+      //  boton.addEventListener('click', () => { agregarAlCarrito(`${x.id}`) }) //
 
+        boton.addEventListener('click', ()=>{
+            agregarAlCarrito(productoNuevo.id)
+         })
+    })
 }
+
 // no pude hacer que el contador de un mismo ítem lo cuente como dos con length++, si bien entiendo que esta parte del código es correcta //
 
 function agregarAlCarrito(id) {
     let validar = carritoDeCompras.some(x => x.id == id)
+    console.log(validar)
+    
     if (validar) {
-        contadorCarrito.innerText = carritoDeCompras.length++
+        console.log(carritoDeCompras)
+        let count = document.getElementById(`cantidad${id}`)
+        // contadorCarrito.innerText = carritoDeCompras.length++ //
+       
+        //linea nueva de codigo//
+
+        carritoDeCompras.map(x=>{
+            if(x.id == id){
+            count.innerText = `cantidad: ${x.cantidad += 1}`
+            actualizarCarrito()
+        }
+    })
+        
     }
     else {
         let productoAgregar = stockProductos.filter((el) => el.id == id)[0]
         console.log(productoAgregar)
         carritoDeCompras.push(productoAgregar)
+      //nueva linea//
+      console.log(carritoDeCompras)
         actualizarCarrito()
 
         let div = document.createElement('div')
@@ -53,6 +77,16 @@ function agregarAlCarrito(id) {
 
         contenedorCarrito.appendChild(div)
 
+        //nuevas lineas//
+
+        let count = document.getElementById(`cantidad${id}`)
+        carritoDeCompras.map(x=>{
+            if(x.id == id){
+            count.innerText = `cantidad: ${x.cantidad = 1}`
+            actualizarCarrito()
+            }
+        })
+
         let botonEliminar = document.getElementById(`eliminar ${productoAgregar.id}`)
         botonEliminar.addEventListener('click', () => {
             botonEliminar.parentElement.remove()
@@ -61,29 +95,44 @@ function agregarAlCarrito(id) {
             console.log(carritoDeCompras)
             actualizarCarrito()
 
-        })}
+        })
     }
+}
 
     function actualizarCarrito() {
-        contadorCarrito.innerText = carritoDeCompras.length
+      //  contadorCarrito.innerText = carritoDeCompras.length //
+
+      //nueva linea//
+      contadorCarrito.innerText = carritoDeCompras.reduce((acc, el) => acc + el.cantidad, 0)
+
         localStorage.setItem('carritoDeCompras', JSON.stringify(carritoDeCompras))
         precioTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + el.precio, 0)
     }
 
-   /* function revisarLocal () {
+   function revisarLocal () {
         let carritoLocal = JSON.parse(localStorage.getItem('carritoDeCompras'))
         if (carritoLocal){
             carritoLocal.forEach((el)=> agregarAlCarrito(el.id))
         }
     }
 
-    revisarLocal () */
+    revisarLocal () 
 
-    let MiFormulario = document.getElementById("EnviarFormulario");
-MiFormulario.addEventListener ("submit", validarFormulario);
+*/
 
-function validarFormulario (evt){
-    evt.preventDefault ();
-    console.log(formulario.children [0].value);
-    console.log(formulario.children [1].value);
-}
+$(document).ready(function(){
+
+    $("#AbrirFormulario").click(function () {
+    $("#exampleModal").modal();
+    });
+    
+    $("#EliminarFormulario").click(function () {
+    setTimeout (function() {
+    location.reload(); }, 1000);
+    });
+
+    $("#EnviarFormulario").click(function () {
+        setTimeout (function() {
+        location.reload(); }, 1000);
+        });
+});
